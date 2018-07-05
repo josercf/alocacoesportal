2cdaead23facba8226b52cb2fb1c322b52ef9c70
José Romualdo da Costa Filho <jose.filho@contax.com.br>
2018-07-05 19:07:34 -0300

    features

--- src/app/pages/recurso-add/recurso-add.component.ts
+++ src/app/pages/recurso-add/recurso-add.component.ts
@@ -5,6 +5,9 @@ import { Recurso } from '../../models/recurso';
 import { CampusService } from '../../services/campus-service';
 import { Campus } from '../../models/campus';
 import { Area } from '../../models/area';
+import { RecursoService } from '../../services/recurso-service';
+import { ModalService } from '../../services/modal-service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recurso-add',
@@ -16,33 +19,53 @@ export class RecursoAddComponent implements OnInit {
   disciplinas: Disciplina[];
   campus: Campus[];
   area: Area[];
-  
+
+  custom: any ={};
+
+
   recurso: Recurso = new Recurso();
   novoTelefone: string;
 
   tabActive: string = "";
   tabs: any[] = [];
 
-  constructor(private disciplinaService: DisciplinaService,
-    private campusService: CampusService) {
+  constructor(private route: ActivatedRoute,
+    private disciplinaService: DisciplinaService,
+    private campusService: CampusService,
+    private recursoService: RecursoService,
+    private modalService: ModalService) {
 
-      this.carregarAreas();
 
-    this.recurso.competencias = [];
-    this.recurso.telefones = [];
+
+    // this.route.params.subscribe(params => {
+    //   if (params['id']) {
+    //     this.recursoService.buscar(params['id'])
+    //       .subscribe(res => this.recurso = res);
+    //   }
+    // });
 
     this.tabs.push(
-      { title: "Competencia", name: "competencia" },
+      { title: "Competência", name: "competencia" },
       { title: "Características", name: "caracteristicas" },
       { title: "Feedbacks", name: "feedbacks" })
+
+    this.tabActive = "competencia";
   }
 
   ngOnInit() {
+
+    this.recurso = this.route.snapshot.data["recurso"] || new Recurso();
+
+    this.custom = this.recurso.area.id;
+
     this.disciplinaService.listar()
       .subscribe(res => this.disciplinas = res);
 
-      this.campusService.listar()
+    this.campusService.listar()
       .subscribe(res => this.campus = res);
+
+    this.carregarAreas();
+
   }
 
   competenciaClick(item: Disciplina) {
@@ -57,27 +80,41 @@ export class RecursoAddComponent implements OnInit {
 
   }
 
-  carregarAreas(){
+  carregarAreas() {
     this.area = [];
-    this.area.push({id: 1, descricao: "INFO"});
-    this.area.push({id: 2, descricao: "ENGLISH"});
-    this.area.push({id: 3, descricao: "PORTUGUÊS"});
+    this.area.push({ id: 1, descricao: "INFO" });
+    this.area.push({ id: 2, descricao: "ENGLISH" });
+    this.area.push({ id: 3, descricao: "PORTUGUÊS" });
   }
 
   tabSelect(tabName: string) {
     this.tabActive = tabName;
   }
 
-  removerTelefone(index: number){
+  removerTelefone(index: number) {
     this.recurso.telefones.splice(index);
   }
 
-  addTelefone(){
+  addTelefone() {
     this.recurso.telefones.push(this.novoTelefone);
     this.novoTelefone = "";
   }
-
+  
   salvar() {
-    alert(JSON.stringify(this.recurso));
+
+    //this.recurso.area = this.area.find()
+
+   this.recurso.area = this.area.find(c => c.id === this.custom);
+    console.log(this.recurso);
+    return;
+
+    if (this.recurso.key) {
+      this.recursoService.alterar(this.recurso)
+        .subscribe(res => this.modalService.show("Sucesso", "Dados salvos!"));
+
+    } else {
+      this.recursoService.criar(this.recurso)
+        .subscribe(res => this.modalService.show("Sucesso", "Dados salvos!"));
+    }
   }
 }
